2c3f458c0f71fd7eb038788915a98d96e9f99214
Andre <andrevieira19992018@gmail.com>
2023-11-14 21:11:48 -0300

    Adcionando arquivos ao repositorio

---
+++ app.js
@@ -0,0 +1,11 @@
+alert("Boas vinda ao jogo mundo secreto !");
+
+// declaração de váriavel
+let numeroSecreto = 5; 
+
+//receber dados 
+let chute = prompt(' Escolha um número entre 1 e 10');
+
+if (chute  ==  numeroSecreto) {
+    console.log('Isso ai! você descobriu o número secreto (5) ');
+}
