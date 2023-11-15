09ea0a3a8a9187c58f33ea850960e833285b26e1
Andre <andrevieira19992018@gmail.com>
2023-11-14 21:38:48 -0300

    Adicionando novos arquivos

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 alert("Boas vinda ao jogo mundo secreto !");
 
 // declaração de váriavel
-let numeroSecreto = 5; 
+let numeroSecreto = 1; 
+console.log(numeroSecreto);
 
 //receber dados 
 let chute = prompt(' Escolha um número entre 1 e 10');
 
 if (chute  ==  numeroSecreto) {
-    console.log('Isso ai! você descobriu o número secreto (5) ');
+    alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} `);
+} else {
+    alert('Você errou :( ')
 }
